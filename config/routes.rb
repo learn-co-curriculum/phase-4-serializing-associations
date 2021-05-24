bb10370d71b1ea35d7cc655540172f45f0f44c96
Liz Burton <liz.burton147@gmail.com>
2021-05-24 12:45:19 -0400

    update starter code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   get '/movies/:id/summary', to: 'movies#summary'
   get '/movie_summaries', to: 'movies#summaries'
+  resources :directors, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
